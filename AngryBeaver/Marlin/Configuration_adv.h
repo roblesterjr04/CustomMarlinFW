fbaf6fcdc468c02584525771a127538ef680fe5d
Rob Lester <rob@robs-macbook-air.local>
2022-11-10 19:40:22 +0000

    updates.

--- AngryBeaver/Marlin/Configuration_adv.h
+++ AngryBeaver/Marlin/Configuration_adv.h
@@ -3438,8 +3438,8 @@
  *
  * See https://marlinfw.org/docs/configuration/2.0.9/laser_spindle.html for more config details.
  */
-//#define SPINDLE_FEATURE
-//#define LASER_FEATURE
+#define SPINDLE_FEATURE
+#define LASER_FEATURE
 #if EITHER(SPINDLE_FEATURE, LASER_FEATURE)
   #define SPINDLE_LASER_ACTIVE_STATE    LOW    // Set to "HIGH" if SPINDLE_LASER_ENA_PIN is active HIGH
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p align="center"><img src="buildroot/share/pixmaps/logo/marlin-outrun-nf-500.png" height="250" alt="MarlinFirmware's logo" /></p>
+<p align="center"><img src="AngryBeaver/buildroot/share/pixmaps/logo/marlin-outrun-nf-500.png" height="250" alt="MarlinFirmware's logo" /></p>
 
 <h1 align="center">Marlin 3D Printer Firmware</h1>
 
